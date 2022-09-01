ca941c10fb8551caabb53730e6c3e2fd39b7de7a
GKolesnikova <dvizenie77@gmail.com>
2022-09-01 09:30:49 +0200

    Verbesserung Method

--- src/main/java/fizzbuzz.java
+++ src/main/java/fizzbuzz.java
@@ -1,12 +1,14 @@
 public class fizzbuzz {
-
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(3));
+    }
 
     public static String fizzBuzz(int number) {
-        if (number == 3) {
+        if (number % 3 == 0) {
             return "fizz";
-        } else if (number == 5) {
+        } else if (number % 5 == 0) {
             return "buzz";
-        } else if (number == 15) {
+        } else if (number % 3 == 0 && number % 5 == 0) {
             return "fizzbuzz";
         } else {
             return String.valueOf(number);
